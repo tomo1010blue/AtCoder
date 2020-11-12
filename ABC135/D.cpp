701629be814b984f7cd020b6c77c67815aad1cc6
宇治知哉 <ujitomoya@utMac.local>
2020-11-12 12:16:05 +0900

    main

--- ABC135/D.cpp
+++ ABC135/D.cpp
@@ -12,7 +12,7 @@ using ll = long long;
 using ld = long double;
 using P = pair<double, double>;
 using G = vector<vector<int>>;
-using mint = modint998244353;
+using mint = modint1000000007;
 const int INF = 1001001001;
 const ll LINF = 1001001001001001001;
 const int dy[] = {-1, 0, 1, 0};
@@ -29,8 +29,22 @@ template<class T> inline bool chmax(T &a, T b){
 
 int main(void)
 {
-	int a, b;
-	cin >> a >> b;
-	cout << 2 * a + 100 - b << endl;
+	string s;
+    cin >> s;
+    int n = s.size();
+    vector<vector<mint>> dp(n+1, vector<mint> (13));
+    dp[0][0] = 1;
+    rep(i, n) {
+        int c;
+        if(s[i] == '?') c = -1;
+        else c = s[i] - '0';
+        rep(j, 10) {
+            if(c != -1 && c != j) continue;
+            rep(ki, 13) {
+                dp[i + 1][(ki * 10 + j) % 13] += dp[i][ki];
+            }
+        }
+    }
+    cout << dp[n][5].val() << endl;
     return 0;
 }
