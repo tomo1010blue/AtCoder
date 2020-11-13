1fd4d127b714a01065bb540976980ee70f26e913
宇治知哉 <ujitomoya@utMac.local>
2020-11-14 08:05:49 +0900

    main

---
+++ ABC137/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, n) repl(i, 0, n)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using P = pair<double, double>;
+using G = vector<vector<int>>;
+using mint = modint1000000007;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main(void)
+{
+	int n;
+    cin >> n;
+    ll ans = 0;
+    map<string, int> mp;
+    rep(i, n) {
+        string s;
+        cin >> s;
+        sort(all(s));
+        ++mp[s];
+    }
+    for(auto x : mp) {
+        int k = x.se;
+        ans += (ll) k * (k - 1) / 2;
+    }
+    cout << ans << endl;
+    return 0;
+}
