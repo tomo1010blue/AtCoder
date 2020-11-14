9667d2c8b06456bf184f3ab086a55eae4c304188
宇治知哉 <ujitomoya@utMac.local>
2020-11-14 10:25:59 +0900

    main

---
+++ ABC140/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, n) repl(i, 0, n)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using P = pair<double, double>;
+using G = vector<vector<int>>;
+using mint = modint1000000007;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main(void)
+{
+	
+    return 0;
+}
