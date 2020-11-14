a8238d3caaef843b72b8f9c439f691426dc46210
宇治知哉 <ujitomoya@utMac.local>
2020-11-14 10:24:20 +0900

    main

---
+++ ABC140/D.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, n) repl(i, 0, n)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using P = pair<double, double>;
+using G = vector<vector<int>>;
+using mint = modint1000000007;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main(void)
+{
+	int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    int score = 0;
+    rep(i, n-1) {
+        if(s[i] == s[i+1]) ++score;
+    }
+    int ans = min(score + 2 * k, n-1);
+    cout << ans << endl;
+    return 0;
+}
