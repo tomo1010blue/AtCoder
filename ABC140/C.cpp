4a357664f9a99ac4eb85eb9e463d1abc803fb5ab
宇治知哉 <ujitomoya@utMac.local>
2020-11-15 11:23:48 +0900

    main

---
+++ ABC140/C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, n) repl(i, 0, n)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using P = pair<double, double>;
+using G = vector<vector<int>>;
+using mint = modint1000000007;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main(void)
+{
+	int n;
+    cin >> n;
+    vector<int> b(n-1);
+	rep(i, n-1) cin >> b[i];
+	int ans = b[0];
+	repl(i, 1, n-1) {
+		ans += min(b[i], b[i-1]);
+	}
+	ans += b[n-2];
+	cout << ans << endl;
+    return 0;
+}
