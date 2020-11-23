6f401913fbe0d25f48f25c6d52334abfa2758f8c
宇治知哉 <ujitomoya@utMac.local>
2020-11-23 10:21:42 +0900

    main

--- ABC184/E.cpp
+++ ABC184/E.cpp
@@ -29,6 +29,49 @@ template<class T> inline bool chmax(T &a, T b){
 }
 
 int main(void) {
-    
+    int h, w;
+    cin >> h >> w;
+    vector<string> a(h);
+    rep(i, h) cin >> a[i];
+    G dist(h, vector<int>(w, INF));
+    queue<P> q;
+    P g;
+    rep(i, h) rep(j, w) {
+        if(a[i][j] == 'S') {
+            q.push({i, j});
+            dist[i][j] = 0;
+        }
+    }
+    vector<vector<P>> warps(256);
+    rep(i, h) rep(j, w) warps[a[i][j]].eb(i, j);
+    while(!q.empty()) {
+        int i = q.front().fi;
+        int j = q.front().se;
+        q.pop();
+        if(a[i][j] == 'G') {
+            cout << dist[i][j] << endl;
+            return 0;
+        }
+        rep(v, 4) {
+            int ni = i + dy[v];
+            int nj = j + dx[v];
+            if(ni < 0 || ni >= h || nj < 0 || nj >= w) continue;
+            if(a[ni][nj] == '#') continue;
+            if(dist[ni][nj] != INF) continue;
+            dist[ni][nj] = dist[i][j] + 1;
+            q.push({ni, nj});
+        }
+        if(islower(a[i][j])) {
+            for(P p : warps[a[i][j]]) {
+                int ni = p.fi;
+                int nj = p.se;
+                if(dist[ni][nj] != INF) continue;
+                dist[ni][nj] = dist[i][j] + 1;
+                q.push({ni, nj});
+            }
+            warps[a[i][j]] = vector<P>();
+        }
+    }
+    cout << -1 << endl;
     return 0;
 }
