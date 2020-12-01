e3addcd3f168e5daea616ac2c23eed4cbf62d915
宇治知哉 <ujitomoya@utMac.local>
2020-12-01 14:39:12 +0900

    main

---
+++ ARC/ARC109/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1000000000;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+    if(a > b){a = b; return true;}
+    return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+    if(a < b){a = b; return true;}
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll n;
+    cin >> n;
+    ll l = 0;
+    ll r = 2 * INF;
+    while(r - l > 1) {
+        ll c = (l + r) / 2;
+        (c * (c + 1) / 2 <= n + 1 ? l : r) = c;
+    }
+    cout << n - l + 1 << '\n';
+    return 0;
+}
