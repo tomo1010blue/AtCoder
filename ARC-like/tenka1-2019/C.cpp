dc6dcdbf55245dbff12030bb7a37ebce7e2f4b62
tomo1010blue <tomoyayuzhi@gmail.com>
2022-12-18 11:54:56 +0900

    mai

---
+++ ARC-like/tenka1-2019/C.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	int ans = INF;
+	vector<int> sum(n+1);
+	sum[0] = 0;
+	rep(i, n) {
+		if(s[i] == '#') sum[i+1] = sum[i]+1;
+		else sum[i+1] = sum[i];
+	}
+	rep(i, n+1) {
+		int cha = (sum[i] - sum[0]) + ((n - i) - (sum[n] - sum[i]));
+		chmin(ans, cha);
+	}
+	cout << ans << endl;
+	return 0;
+}
