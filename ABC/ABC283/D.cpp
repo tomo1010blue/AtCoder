e54abfd853270d12738b8da936fdfe44493fec70
tomo1010blue <tomoyayuzhi@gmail.com>
2022-12-25 10:33:12 +0900

    main

---
+++ ABC/ABC283/D.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	int n = sz(s);
+	vector<set<char>> vset;
+	set<char> t;
+	vset.pb(t);
+	int rank = 0;
+	rep(i, n) {
+		if(s[i] == '(') {
+			vset.pb(vset[rank]);
+			rank++;
+		}else if(s[i] == ')') {
+			vset.pop_back();
+			rank--;
+		} else {
+			if(vset[rank].count(s[i])) {
+				cout << "No" << endl;
+				return 0;
+			}
+			vset[rank].insert(s[i]);
+		}
+	}
+	cout << "Yes" << endl;
+	return 0;
+}
