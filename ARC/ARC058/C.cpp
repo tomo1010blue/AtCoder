5cac36994c41e49a9c065f0ecd69414260cfd3eb
tomo1010blue <tomoyayuzhi@gmail.com>
2023-01-02 11:25:25 +0900

    main

---
+++ ARC/ARC058/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	set<int> d;
+	rep(i, k) {
+		int a;
+		cin >> a;
+		d.insert(a);
+	}
+	int ans = n;
+	while(1) {
+		string s = to_string(ans);
+		bool ok = true;
+		rep(i, sz(s)) {
+			int x = (int) (s[i] - '0');
+			if(d.count(x)) ok = false;
+		}
+		if(ok) {
+			cout << ans << endl;
+			return 0;
+		}
+		ans++;
+	}
+	return 0;
+}
