93b85259505a7f9111dbe2bca1bac9b06203bc63
tomo1010blue <tomoyayuzhi@gmail.com>
2023-01-05 10:30:54 +0900

    main

---
+++ ARC/ARC137/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	rep(i, n) cin >> a[i];
+	int mn = 0, mx = 0, cur = 0, x = 0, y = 0;
+	rep(i, n) {
+		if(a[i] == 0) --cur;
+		else ++cur;
+		chmin(x, cur-mx);
+		chmax(y, cur-mn);
+		chmin(mn, cur);
+		chmax(mx, cur);
+	}
+	cout << y - x + 1 << endl;
+	return 0;
+}
