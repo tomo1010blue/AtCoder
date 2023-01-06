dbe2a906858f42434a219001c240b42ade6c0428
tomo1010blue <tomoyayuzhi@gmail.com>
2023-01-06 11:50:28 +0900

    main

---
+++ ABC/ABC252/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<string> s(n);
+	rep(i, n) cin >> s[i];
+	int ans = INF;
+	rep(i, 10) {
+		char c = '0' + i;
+		int t = 0;
+		map<int,int> mp;
+		rep(j, n) rep(k, 10) if(s[j][k] == c) mp[k]++;
+		for(auto v : mp) chmax(t, 10*(v.se-1)+v.fi);
+		chmin(ans, t);
+	}
+	cout << ans << endl;
+	return 0;
+}
