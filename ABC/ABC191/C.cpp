5f4a0a6c90ca612859324b8c902e77db90b92d29
tomo1010blue <tomoyayuzhi@gmail.com>
2023-01-22 11:35:00 +0900

    main

---
+++ ABC/ABC191/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int h, w;
+	cin >> h >> w;
+	vector<string> s(h);
+	rep(i, h) cin >> s[i];
+	int ans = 0;
+	rep(i, h-1) rep(j, w-1) {
+		int cnt = 0;
+		rep(di, 2) rep(dj, 2) if(s[i+di][j+dj] == '#') ++cnt;
+		if(cnt == 1 || cnt == 3) ++ans;
+	}
+	cout << ans << endl;
+	return 0;
+}
