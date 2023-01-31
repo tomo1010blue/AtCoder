1194152d652372ba1c86503b11e73485227dd5f4
tomo1010blue <tomoyayuzhi@gmail.com>
2023-01-31 17:11:39 +0900

    main

---
+++ ABC/ABC126/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	double ans = 0.;
+	repl(i, 1, n+1) {
+		double cur = (double) 1. / n;
+		int num = 1;
+		int cnt = 0;
+		while(i * num < k) {
+			++cnt;
+			num *= 2;
+		}
+		cur *= pow(0.5, cnt);
+		ans += cur;
+	}
+	cout << setprecision(10) << ans << endl;
+	return 0;
+}
