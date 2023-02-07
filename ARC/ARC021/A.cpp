0d916dc280ea4bb3ae42e999b8a1f058277e5cb6
tomo1010blue <tomoyayuzhi@gmail.com>
2023-02-07 16:50:11 +0900

    main

---
+++ ARC/ARC021/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define fi first
+#define se second
+#define repl(i, l, r) for (int i = l; i < r; ++i)
+#define rep(i, r) repl(i, 0, r)
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define sz(x) ((int) (x).size())
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using mint = modint1000000007;
+using P = pair<int, int>;
+using G = vector<vector<int>>;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, -1, 0, 1};
+template<class T> inline bool chmin(T &a, T b){
+	if(a > b){a = b; return true;}
+	return false;
+}
+template<class T> inline bool chmax(T &a, T b){
+	if(a < b){a = b; return true;}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	G a(4, vector<int>(4));
+	rep(i, 4) rep(j, 4) cin >> a[i][j];
+	rep(i, 4) rep(j, 4) rep(k, 4) {
+		int di = i + dy[k];
+		int dj = j + dx[k];
+		if(di < 0 || di >= 4 || dj < 0 || dj >= 4) continue;
+		if(a[i][j] == a[di][dj]) {
+			cout << "CONTINUE" << endl;
+			return 0;
+		}
+	}
+	cout << "GAMEOVER" << endl;
+	return 0;
+}
